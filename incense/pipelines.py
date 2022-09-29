2a53b3f2279981c1c2e59ee6f6076211c1237b6c
Vlad <vlad.bashtannyk11@gmail.com>
2022-09-29 11:38:00 +0300

    Updated pipelines.py

--- incense/pipelines.py
+++ incense/pipelines.py
@@ -5,43 +5,48 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 import psycopg2
+import logging
+from decouple import config
 
 
 class IncensePipeline:
     def __init__(self):
-        hostname = '127.0.0.1'
-        username = 'vladyslav'
-        password = 'Kannibal21_'
-        database = 'incense'
+        hostname = config('HOSTNAME')
+        username = config('USERNAME')
+        password = config('PASSWORD')
+        database = config('DATABASE')
 
         self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
         self.cur = self.connection.cursor()
+        logging.debug("CREATING DATABASE")
         self.cur.execute("""
                 CREATE TABLE IF NOT EXISTS incense(
                     id serial PRIMARY KEY, 
                     title TEXT,
                     image_link TEXT,
+                    deep_link TEXT,
                     opt_price NUMERIC,
                     drop_price NUMERIC,
                     discount_price NUMERIC,
                     currency VARCHAR(30),
-                    status VARCHAR(30)
+                    status VARCHAR(30),
+                    datetime VARCHAR(30)
                 )
                 """)
 
     def process_item(self, item, spider):
-        self.cur.execute("select * from incense where title = '%s'" % item['title'])
+        self.cur.execute("SELECT * FROM incense WHERE title = '%s'" % item['title'])
         result = self.cur.fetchone()
         if result:
-            spider.logger.warn("Item already in database: '%s'" % item['title'])
-            self.cur.execute("UPDATE incense SET status = 'OLD' where title = '%s'" % (item['title'],))
+            logging.debug("ITEM IS ALREADY IN EXIST: '%s'" % item['title'])
+            self.cur.execute("UPDATE incense SET status = 'OLD' WHERE title = '%s'" % (item['title'],))
         else:
-            self.cur.execute("insert into incense (title, image_link, opt_price, drop_price, discount_price, currency) "
-                             "values ('%s','%s',%s,%s,%s,'%s')" %
-                             (item["title"], item["image_link"], item["opt_price"], item["drop_price"],
-                              item["discount_price"], item["currency"]))
+            logging.debug("INSERTING ITEM")
+            self.cur.execute("INSERT INTO incense "
+                             "(title, image_link, deep_link, opt_price, drop_price, retail_price, currency, "
+                             "status, datetime) VALUES ('%(title)s','%(image_link)s','%(deep_link)s',%(opt_price)s,"
+                             "%(drop_price)s,%(retail_price)s,'%(currency)s','%(status)s''%(datetime)s')" % item)
             self.connection.commit()
         return item
 
