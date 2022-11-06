cfb0a21e409ff92f4a94ddd3ab68d70d1787253e
Vlad <vlad.bashtannyk11@gmail.com>
2022-11-06 11:28:36 +0200

    Added session type and secret key

--- flask_app/settings.py
+++ flask_app/settings.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+from credentials import SECRET_KEY
 
 APP = Flask(__name__)
 API = Api(APP)
+
+
+APP.config['SESSION_TYPE'] = 'memcached'
+APP.config['SECRET_KEY'] = SECRET_KEY
