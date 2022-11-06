78be6695c5a44ae2de72e33c27671d0caa09a2b2
Vlad <vlad.bashtannyk11@gmail.com>
2022-11-06 15:08:17 +0200

    Updated decorators

--- flask_app/decorators.py
+++ flask_app/decorators.py
@@ -1,12 +1,13 @@
 from functools import wraps
 from flask import session, redirect
+from flask_api import status
 
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get('is_logged_in') is None:
-            return redirect('/login', code=302)
+        if session.get('is_logged_in') is False:
+            return redirect('/login', code=status.HTTP_302_FOUND)
         return f(*args, **kwargs)
     return decorated_function
 
@@ -14,7 +15,7 @@ def login_required(f):
 def logout_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get('is_logged_in') is not None:
-            return redirect('/incense', code=302)
+        if session.get('is_logged_in') is True:
+            return redirect('/incense', code=status.HTTP_302_FOUND)
         return f(*args, **kwargs)
     return decorated_function
