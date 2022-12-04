bd47b746754f3538922c98e8232b34c55f0a4dd5
Vlad <vlad.bashtannyk11@gmail.com>
2022-12-04 09:54:33 +0200

    Added credential getter

---
+++ flask_app/credentials.py
@@ -0,0 +1,8 @@
+from decouple import config
+
+
+HOSTNAME = config('HOSTNAME')
+USERNAME = config('USERNAME')
+PASSWORD = config('PASSWORD')
+DATABASE = config('DATABASE')
+SECRET_KEY = config('SECRET_KEY')
