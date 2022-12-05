eae5cc3fafb99d2289ac3517b29dfbcfddedfb92
Vlad <vlad.bashtannyk11@gmail.com>
2022-12-05 17:43:35 +0200

    Added signup page

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 from flask_app.settings import APP, API
 from flask_app.scrapy_side import ScrapySide
 from flask_app.database_dispatcher import DatabaseDispatcher
+from flask_app.models import Users
 from flask import redirect, url_for, session
 from decorators import logout_required, login_required
 
@@ -16,12 +17,12 @@ def resource_not_found(request):
 class MainPage(Resource):
     DEFAULT_HEADERS = {'Content-Type': 'text/html'}
 
-    # @login_required
+    @login_required
     def get(self):
         rendered_result = render_template("home_page.html")
         return make_response(rendered_result, status.HTTP_200_OK, self.DEFAULT_HEADERS)
 
-    # @login_required
+    @login_required
     def post(self):
         extract_args = request.form if "url" in request.form else None
         show_args = request.form if "limit" in request.form else None
@@ -37,6 +38,26 @@ class MainPage(Resource):
             return make_response(rendered_result, status.HTTP_200_OK, self.DEFAULT_HEADERS)
         else:
             return {"result": "BAD REQUEST!"}, status.HTTP_400_BAD_REQUEST
+
+
+class SignUpPage(Resource):
+    DEFAULT_HEADERS = {'Content-Type': 'text/html'}
+
+    @logout_required
+    def get(self):
+        rendered_result = render_template("signup_page.html", result={})
+        return make_response(rendered_result, status.HTTP_200_OK, self.DEFAULT_HEADERS)
+
+    @logout_required
+    def post(self):
+        log_in_args = request.form
+        result = Users().create_user(log_in_args)
+        if result.get("error"):
+            rendered_result = render_template("signup_page.html", result=result)
+            session["is_logged_in"] = True
+            return make_response(rendered_result, status.HTTP_403_FORBIDDEN, self.DEFAULT_HEADERS)
+        else:
+            return redirect(url_for("incense"), code=status.HTTP_302_FOUND)
 
 
 class LoginPage(Resource):
@@ -78,6 +99,7 @@ class LogoutPage(Resource):
 
 
 API.add_resource(MainPage, "/incense", endpoint="incense")
+API.add_resource(SignUpPage, "/signup", endpoint="signup")
 API.add_resource(LoginPage, "/login", endpoint="login")
 API.add_resource(LogoutPage, "/logout", endpoint="logout")
 
